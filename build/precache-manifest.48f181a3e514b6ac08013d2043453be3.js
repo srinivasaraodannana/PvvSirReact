eafd82f8b169bc4e042d9fd7b124e5265861a5fd
DSK9012 <srinivasarao.dannana2000@gmail.com>
2023-03-09 01:41:15 +0530

    kj
    kn

---
+++ build/precache-manifest.48f181a3e514b6ac08013d2043453be3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "de978d757cd160761bb2",
+    "url": "/static/css/main.852f3ac6.chunk.css"
+  },
+  {
+    "revision": "de978d757cd160761bb2",
+    "url": "/static/js/main.de978d75.chunk.js"
+  },
+  {
+    "revision": "bcd620d97769308d2936",
+    "url": "/static/js/1.bcd620d9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "da5d22db468a518ae21444bf180d51c7",
+    "url": "/index.html"
+  }
+];
